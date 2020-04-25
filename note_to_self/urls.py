7b16154c30c0f0bc3e9b059330d6dd3d6c73fdb4
Hayata Sato <hayatasato.0@gmail.com>
2020-04-25 14:06:17 +0900

    Update page title

--- note_to_self/urls.py
+++ note_to_self/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.views.decorators.cache import cache_control
 from django.conf.urls.static import static
 from django.conf import settings
@@ -11,7 +10,6 @@ urlpatterns = [
     path('', include('book_manager.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('book_manager/sw.js', (TemplateView.as_view(template_name="sw.js", content_type='application/javascript', )), name='sw.js'),
     path('markdownx/', include('markdownx.urls')),
 ]
 
